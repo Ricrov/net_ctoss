c0057811b00edcc99d429a2ea74d498f86c6b7df
Ricrov <ric.hou@hotmail.com>
2018-11-03 16:14:35 +0800

    Base Project

---
+++ java/src/main/java/com/ric/netctoss/NetctossApplication.java
@@ -0,0 +1,12 @@
+package com.ric.netctoss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetctossApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetctossApplication.class, args);
+    }
+}
---
+++ java/src/main/java/com/ric/netctoss/config/ShiroConfig.java
@@ -0,0 +1,4 @@
+package com.ric.netctoss.config;
+
+public class ShiroConfig {
+}
